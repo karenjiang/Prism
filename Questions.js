c16832bda584e714710a2e0fe1efb0dba2f4d8b5
ja-San <jake_sanders@horacemann.org>
2016-05-19 20:21:55 -0400

    Restarted in JavaScript
    I still have the Ionic on my desktop and we can always redownload it
    from the Ionic website, but starting in JavaScript anew

---
+++ Questions.js
@@ -0,0 +1,102 @@
+var questions = [
+	[],
+	[
+		//100 points
+		{
+			question:"For how many years was Odysseus away?",
+			a:"One month",
+			b:"One year",
+			c:"Ten years",
+			d:"Twenty years",
+			correctAnswer:"c"
+		},
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+	],
+
+	[
+		//200 points
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+	],
+	
+	[
+		//300 points
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+	],
+	
+	[
+		//400 points
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+	],
+	
+	[
+		//500 points
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+		{
+			question:" ... ",
+			a:" ... ",
+			b:" ... ",
+			c:" ... ",
+			d:" ... ",
+			correctAnswer:" ... "
+		},
+	]
+]
